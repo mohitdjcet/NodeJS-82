f4685f9322ec057b30dae8e9c9e0b4d712ecae3f
Mohit Kumar <mks.djcet@gmail.com>
2026-03-17 21:42:54 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,48 +1,13 @@
-// function loginUser(callback){
-//     setTimeout(()=>{
-//         console.log("User Logged in")
-//         callback();
-//     },3000);
-// }
+import axios from "axios";
 
-// function getProfile(callback){
-//     setTimeout(()=>{
-//         console.log("getProfile")
-//         callback();
-//     },3000);
-// }
-
-// function getPost(callback){
-//     setTimeout(()=>{
-//         console.log("getPost")
-//         callback();
-//     },3000);
-// }
-
-// loginUser(()=>{
-//     getProfile(()=>{
-//         getPost(()=>{
-//             console.log("All Task Done")
-//         })
-//     })
-// })
-
-function step1(){
-    return Promise.resolve("Step 1 completed")
-}
-function step2(){
-    return Promise.resolve("Step 2 completed")
-}
-function step3(){
-    return Promise.resolve("Step 3 completed")
+async function getUsers(){
+    try{
+        const res = await axios.get("https://jsonplaceholder.typicode.com/users");
+        console.log("User Data:");
+        console.log(res.data);
+    } catch (error){
+        console.log("Error",error.message); 
+    }
 }
 
-step1().then((result)=>{
-    console.log(result);
-    return step2();
-}).then((result)=>{
-    console.log(result);
-    return step3();
-}).then((result)=>{
-    console.log(result);
-})
+getUsers()
