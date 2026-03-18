3c80206abf2a44a1e9e18d778a702ee595818d78
Mohit Kumar <mks.djcet@gmail.com>
2026-03-18 16:58:48 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-import axios from "axios";
+import express from "express";
 
-async function getUsers(){
-    try{
-        const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-        console.log("User Data:");
-        console.log(res.data);
-    } catch (error){
-        console.log("Error",error.message); 
-    }
-}
+const app = express();
+const PORT = 3001;
 
-getUsers()
+//MiddleWare
+app.use(express.json)
+
+//Home Route
+app.get("/",(req,res)=>{
+    res.send("Hello from Express")
+})
+
+app.get("/contact",(req,res)=>{
+    res.send("<h1>contact Page</h1>")
+})
+
+app.post("/data",(req,res)=>{
+    const userData = req.body;
+
+    res.json({
+        message:"Data received fine",
+        data: userData
+    })
+})
+
+//Server
+app.listen(PORT,()=>{
+    console.log("Server running on 3001 Port");
+})
