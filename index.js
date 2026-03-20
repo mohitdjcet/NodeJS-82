911ee54925fd18419413cfedebfa4529e0a88910
Mohit Kumar <mks.djcet@gmail.com>
2026-03-20 21:41:44 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,30 +1,75 @@
 import express from "express";
 
 const app = express();
-const PORT = 3001;
 
-//MiddleWare
-app.use(express.json)
+app.use(express.json());
 
-//Home Route
-app.get("/",(req,res)=>{
-    res.send("Hello from Express")
+let users=[
+    {id:1,name:"Mohit"},
+    {id:2,name:"Rohit"}
+]
+
+app.delete("/delete-user/:id",(req,res)=>{
+    const userId = parseInt(req.params.id);
+    const newUser = users.filter(u => u.id !== userId);
+
+    if(newUser.length !== users.length){
+        users = newUser;
+
+        res.json({
+            message: "User deleted Successfully",
+            users
+        })
+    } else{
+        res.status(404).json({
+        message:"User Not Found"
+        })
+    }
 })
 
-app.get("/contact",(req,res)=>{
-    res.send("<h1>contact Page</h1>")
+app.put("/update-user/:id",(req,res)=>{
+    const userId = parseInt(req.params.id);
+    const newName = req.body.name;
+
+    const user = users.find(u => u.id === userId);
+
+    if(user){
+        user.name = newName;
+
+        res.json({
+            message:"User Updated fine",
+            users
+        });
+    } else {
+        res.status(404).json({
+            message:"User Not Found"
+        })
+    }
 })
 
-app.post("/data",(req,res)=>{
-    const userData = req.body;
+app.post("/add-user",(req,res)=>{
+    const user = req.body;
 
     res.json({
-        message:"Data received fine",
-        data: userData
+        message:"User added successfully",
+        user:user
     })
 })
 
-//Server
-app.listen(PORT,()=>{
-    console.log("Server running on 3001 Port");
+app.get("/",(req,res)=>{
+    res.send("Welcome to Home Page")
+})
+
+app.get("/users",(req,res)=>{
+    res.json(users)
+})
+
+app.get("/search",(req,res)=>{
+    const name = req.query.name;
+    res.send(`Seraching for ${name}`)
+})
+
+app.listen(3001,()=>{
+    console.log("Server is running on port 3001");
+    
 })
