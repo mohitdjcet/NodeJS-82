fe96e3ea3a6ced7ce6269089afa0be7db87c3f08
Mohit Kumar <mks.djcet@gmail.com>
2026-03-28 21:32:03 +0530

    Updated code

--- index.js
+++ index.js
@@ -1,45 +1,39 @@
-import express from 'express';
+import express from "express";
+import { MongoClient } from "mongodb";
 
 const app = express();
+const PORT = 5001;
 
-// //Static folder path
-// app.use(express.static("public"));
+//MongoDB URL
+const url = "mongodb://localhost:27017";
 
-// app.get("/",(req,res)=>{
-//     throw new Error("Something went wrong")
-// })
+//Create Client
+const client= new MongoClient(url);
 
-// //Error Middleware
-// app.use((err,req,res,next)=>{
-//     console.log(err.message);
+//DB NAME
+const dbName = "myApp";
 
-//     res.status(500).json({
-//         message:"Internal Server Error"
-//     })
-// }) 
+let db;
 
-app.get("/",(req,res)=>{
-    res.status(200).send("Sucesss")
+//Connect to MongoDB
+async function connectDB(){
+    try{
+        await client.connect();
+        console.log("MD Connected");
+        db = client.db(dbName)
+    } catch(err){
+        console.log("Connection Error:", err)
+    }
+}
+
+//Route
+app.get("/", async (req,res)=>{
+    const users = await db.collection("users").find().toArray();
+    res.json(users);
 })
 
-app.get("/not-found",(req,res)=>{
-    res.status(401).json({
-        message:"Page Not Found"
-    })
+//Server start
+app.listen(PORT, async ()=>{
+    await connectDB();
+    console.log("server is running port", PORT)
 })
-
-app.get("/error",(req,res)=>{
-    res.status(500).json({
-        message:"Internal server ereror"
-    })
-})
-
-app.listen(3001);
-
-//Range: 1**-Informatinal
-//2**-Sucess
-//3**-Redirect
-//4**-Client Error
-//5**- Server Error
-
-
--- public/script.js
+++
@@ -1,3 +0,0 @@
-function showMsg(){
-    alert("Welcome to Static file")
-}
