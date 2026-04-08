a3c9fe43003ab211ff007a2975154f98172e1dc5
Mohit Kumar <mks.djcet@gmail.com>
2026-04-08 17:04:48 +0530

    Uodated code

--- index.js
+++ index.js
@@ -1,30 +1,73 @@
-// //fs - file system module //core module
-// const fs = require('fs');
+import express from 'express';
+import mongoose from 'mongoose';
 
-// console.log("Start");
+const app = express();
+const PORT = 5001;
 
-// fs.readFile("data.txt","utf8",(err,data)=>{
-//     if(err){
-//         console.log("Error reading file",err);
-//         return;
-//     }
-//     console.log(data);
-// })
+app.use(express.json());
 
-// // const data = fs.readFileSync("data.txt","utf8");
-// // console.log(data);
+//Connect to Mongoose(DB Connection)
+mongoose.connect('mongodb://localhost:27017/myApp')
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((err) => {
+    console.error('Error connecting to MongoDB', err);
+});
 
-// console.log("End");
-
-const fs = require('fs').promises;
-
-async function readData(){
-    try{
-        const data = await fs.readFile("data.txt","utf8");
-        console.log(data);
-    } catch(err){
-        console.log("Error",err);
+//Schema
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    age: Number,
+    email: {
+        type: String,
+        required: true,
+        unique: true
     }
-}
+});
 
-readData();
+//Model
+const User = mongoose.model('User', userSchema);
+
+//CREATE User post API
+app.post('/users', async (req, res) => {
+    const users = new User(req.body);
+    const saved = await users.save();
+    res.json(saved);
+});
+
+//Read
+app.get('/users', async (req, res) => {
+    const users = await User.find();
+    res.json(users);
+});
+
+//Read by ID
+app.get('/users/:id', async (req, res) => {
+    const user = await User.findById(req.params.id);
+    res.json(user);
+});
+
+//Update
+app.put('/users/:id', async (req, res) => {
+    const updated = await User.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+    res.json(updated);
+});
+
+//Delete
+app.delete('/users/:id', async (req, res) => {
+    await User.findByIdAndDelete(req.params.id);
+    res.json({ message: 'User deleted' });
+})
+
+// Server Start
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
